3dff1e2a6c364367f7b6e1e26559239458546db4
liujing <liujing@windimg.com>
2020-08-08 07:09:14 +0800

    format message

--- main.py
+++ main.py
@@ -43,26 +43,29 @@ class QuitStrategy:
 
         if cur_price < cost:
             per = (cost - cur_price) / cost
-            msg = '亏损ing...name = {}, cost = {}\ncur = {}, high = {}\ncur_per = -{:.2f}%\n'.format(info['name'], info['cost'], cur_price, high_price, per * 100)
-            print(msg)
+            msg = self.tip_message(False, info['name'], info['cost'], cur_price, high_price, per * 100, per* 100)
+
             if per >= 0.03:
-                msg = '亏损ing...name = {}, cost = {}\n\ncur = {}, high = {}\n\ncur_per = -{:.2f}%'.format(info['name'], info['cost'], cur_price, high_price, per * 100)
                 WarnBox(msg)
 
             ui_callback(code, msg)
         else:
             high_per = (high_price - cost) / cost
             cur_per = (cur_price - cost) / cost
-            msg = '盈利ing...name = {}, cost = {}\ncur = {}, high = {}\ncur_per = {:.2f}%, high_per = {:.2f}%\n'.format(info['name'], info['cost'], cur_price, high_price, cur_per * 100, high_per* 100)
-            print(msg)
 
+            msg = self.tip_message(True, info['name'], info['cost'], cur_price, high_price, cur_per * 100, high_per* 100)
             pairs = [[0.05, 0.03], [0.07, 0.04], [0.10, 0.06], [0.15, 0.1], [0.20, 0.14], [0.30, 0.23], [0.40, 0.31], [0.50, 0.40], [0.60, 0.48]]
             for pair in pairs:
                 if high_per >= pair[0] and cur_per <= pair[1]:
-                    msg = '盈利ing... name = {}, cost = {}\n\ncur = {}, high = {}\n\ncur_per = {:.2f}%, high_per = {:.2f}%'.format(info['name'], info['cost'], cur_price, high_price, cur_per * 100, high_per* 100)
                     WarnBox(msg)
                     break
             ui_callback(code, msg)
+
+    def tip_message(self, is_earn, name, cost, cur_price, high_price, cur_per, high_per):
+        format_msg = '盈利ing... name = {}, cost = {}\ncur = {}, high = {}\ncur_per = {:.2f}%, high_per = {:.2f}%'.format(name, cost, cur_price, high_price, cur_per * 100, high_per* 100))
+        if not is_earn:
+            format_msg = '亏损ing...name = {}, cost = {}\ncur = {}, high = {}\ncur_per = -{:.2f}%'.format(name, cost, cur_price, high_price, per * 100)
+        return format_msg
 
 class RepeatingTimer(Timer): 
     def run(self):
@@ -126,7 +129,6 @@ if __name__ == '__main__':
         mw = Watch(root)
         mywatch = Button(frame1, text = '走你...', command = mw.start)
         mywatch.pack(side = LEFT)
-        mw.Bind(wx.EVT_CLOSE, mw.OnClose)
 
         root.mainloop()
     main()
